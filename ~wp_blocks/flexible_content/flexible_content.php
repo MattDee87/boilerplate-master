40bf69a0eed312cd0f5730022bcf6735b5e4f6c8
MattDee87 <103971522+MattDee87@users.noreply.github.com>
2026-04-23 22:38:47 -0400

    Bug fixes to Flexible Content
    Image is not rendering to the left or right of content above cards

--- ~wp_blocks/flexible_content/flexible_content.php
+++ ~wp_blocks/flexible_content/flexible_content.php
@@ -39,74 +39,154 @@
 <div class="flexible_content_section <?= esc_attr( trim( "$layout_class $bg_class $align_class $media_class" ) ); ?>">
     <div class="fcs_wrapper">
 
-        <div class="fcs_content">
+        <?php if ( $layout === 'split' ) : ?>
 
-            <?php if ( $eyebrow ) : ?>
-                <p class="fcs_eyebrow"><?= esc_html( $eyebrow ); ?></p>
-            <?php endif; ?>
+            <div class="fcs_split_row">
 
-            <h2 class="fcs_heading"><?= esc_html( $heading ); ?></h2>
+                <div class="fcs_content">
 
-            <?php if ( $body ) : ?>
-                <div class="fcs_body"><?= wp_kses_post( $body ); ?></div>
-            <?php endif; ?>
-
-            <?php if ( ( $cta_1_label && $cta_1_url ) || ( $cta_2_label && $cta_2_url ) ) : ?>
-                <div class="fcs_ctas">
-                    <?php if ( $cta_1_label && $cta_1_url ) : ?>
-                        <a href="<?= esc_url( $cta_1_url ); ?>" class="btn"><?= esc_html( $cta_1_label ); ?></a>
+                    <?php if ( $eyebrow ) : ?>
+                        <p class="fcs_eyebrow"><?= esc_html( $eyebrow ); ?></p>
                     <?php endif; ?>
-                    <?php if ( $cta_2_label && $cta_2_url ) : ?>
-                        <a href="<?= esc_url( $cta_2_url ); ?>" class="btn btn-outline"><?= esc_html( $cta_2_label ); ?></a>
+
+                    <h2 class="fcs_heading"><?= esc_html( $heading ); ?></h2>
+
+                    <?php if ( $body ) : ?>
+                        <div class="fcs_body"><?= wp_kses_post( $body ); ?></div>
                     <?php endif; ?>
+
+                    <?php if ( ( $cta_1_label && $cta_1_url ) || ( $cta_2_label && $cta_2_url ) ) : ?>
+                        <div class="fcs_ctas">
+                            <?php if ( $cta_1_label && $cta_1_url ) : ?>
+                                <a href="<?= esc_url( $cta_1_url ); ?>" class="btn"><?= esc_html( $cta_1_label ); ?></a>
+                            <?php endif; ?>
+                            <?php if ( $cta_2_label && $cta_2_url ) : ?>
+                                <a href="<?= esc_url( $cta_2_url ); ?>" class="btn btn-outline"><?= esc_html( $cta_2_label ); ?></a>
+                            <?php endif; ?>
+                        </div>
+                    <?php endif; ?>
+
                 </div>
-            <?php endif; ?>
 
-        </div>
-
-        <?php if ( $media_type === 'image' && $image_url ) : ?>
-            <div class="fcs_media">
-                <img src="<?= esc_url( $image_url ); ?>" alt="<?= esc_attr( $image_alt ); ?>">
-            </div>
-        <?php elseif ( $media_type === 'video' && $media_video_url ) : ?>
-            <div class="fcs_media">
-                <div class="embed-responsive">
-                    <iframe src="<?= esc_url( $media_video_url ); ?>" allowfullscreen></iframe>
-                </div>
-            </div>
-        <?php endif; ?>
-
-        <?php if ( $items && is_array( $items ) && count( $items ) > 0 ) : ?>
-            <div class="fcs_items fcs_items_count_<?= count( $items ); ?>">
-                <?php foreach ( $items as $item ) :
-                    $type    = isset( $item['item_type'] )       ? $item['item_type']       : 'card';
-                    $i_head  = isset( $item['item_heading'] )    ? $item['item_heading']    : '';
-                    $i_num   = isset( $item['item_number'] )     ? $item['item_number']     : '';
-                    $i_text  = isset( $item['item_text'] )       ? $item['item_text']       : '';
-                    $i_link  = isset( $item['item_link'] )       ? $item['item_link']       : '';
-                    $i_label = isset( $item['item_link_label'] ) ? $item['item_link_label'] : '';
-                ?>
-                    <div class="fcs_item fcs_item_type_<?= esc_attr( $type ); ?>">
-
-                        <?php if ( $type === 'stat' && $i_num ) : ?>
-                            <p class="fcs_item_number"><?= esc_html( $i_num ); ?></p>
-                        <?php endif; ?>
-
-                        <?php if ( $i_head ) : ?>
-                            <h3 class="fcs_item_heading"><?= esc_html( $i_head ); ?></h3>
-                        <?php endif; ?>
-
-                        <?php if ( $i_text ) : ?>
-                            <p class="fcs_item_text"><?= esc_html( $i_text ); ?></p>
-                        <?php endif; ?>
-
-                        <?php if ( $type === 'linked_card' && $i_link && $i_label ) : ?>
-                            <a href="<?= esc_url( $i_link ); ?>" class="fcs_item_link"><?= esc_html( $i_label ); ?></a>
-                        <?php endif; ?>
-
+                <?php if ( $media_type === 'image' && $image_url ) : ?>
+                    <div class="fcs_media">
+                        <img src="<?= esc_url( $image_url ); ?>" alt="<?= esc_attr( $image_alt ); ?>">
                     </div>
-                <?php endforeach; ?>
+                <?php elseif ( $media_type === 'video' && $media_video_url ) : ?>
+                    <div class="fcs_media">
+                        <div class="embed-responsive">
+                            <iframe src="<?= esc_url( $media_video_url ); ?>" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
             </div>
+
+            <?php if ( $items && is_array( $items ) && count( $items ) > 0 ) : ?>
+                <div class="fcs_items fcs_items_count_<?= count( $items ); ?>">
+                    <?php foreach ( $items as $item ) :
+                        $type    = isset( $item['item_type'] )       ? $item['item_type']       : 'card';
+                        $i_head  = isset( $item['item_heading'] )    ? $item['item_heading']    : '';
+                        $i_num   = isset( $item['item_number'] )     ? $item['item_number']     : '';
+                        $i_text  = isset( $item['item_text'] )       ? $item['item_text']       : '';
+                        $i_link  = isset( $item['item_link'] )       ? $item['item_link']       : '';
+                        $i_label = isset( $item['item_link_label'] ) ? $item['item_link_label'] : '';
+                    ?>
+                        <div class="fcs_item fcs_item_type_<?= esc_attr( $type ); ?>">
+
+                            <?php if ( $type === 'stat' && $i_num ) : ?>
+                                <p class="fcs_item_number"><?= esc_html( $i_num ); ?></p>
+                            <?php endif; ?>
+
+                            <?php if ( $i_head ) : ?>
+                                <h3 class="fcs_item_heading"><?= esc_html( $i_head ); ?></h3>
+                            <?php endif; ?>
+
+                            <?php if ( $i_text ) : ?>
+                                <p class="fcs_item_text"><?= esc_html( $i_text ); ?></p>
+                            <?php endif; ?>
+
+                            <?php if ( $type === 'linked_card' && $i_link && $i_label ) : ?>
+                                <a href="<?= esc_url( $i_link ); ?>" class="fcs_item_link"><?= esc_html( $i_label ); ?></a>
+                            <?php endif; ?>
+
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
+
+        <?php else : ?>
+
+            <div class="fcs_content">
+
+                <?php if ( $eyebrow ) : ?>
+                    <p class="fcs_eyebrow"><?= esc_html( $eyebrow ); ?></p>
+                <?php endif; ?>
+
+                <h2 class="fcs_heading"><?= esc_html( $heading ); ?></h2>
+
+                <?php if ( $body ) : ?>
+                    <div class="fcs_body"><?= wp_kses_post( $body ); ?></div>
+                <?php endif; ?>
+
+                <?php if ( ( $cta_1_label && $cta_1_url ) || ( $cta_2_label && $cta_2_url ) ) : ?>
+                    <div class="fcs_ctas">
+                        <?php if ( $cta_1_label && $cta_1_url ) : ?>
+                            <a href="<?= esc_url( $cta_1_url ); ?>" class="btn"><?= esc_html( $cta_1_label ); ?></a>
+                        <?php endif; ?>
+                        <?php if ( $cta_2_label && $cta_2_url ) : ?>
+                            <a href="<?= esc_url( $cta_2_url ); ?>" class="btn btn-outline"><?= esc_html( $cta_2_label ); ?></a>
+                        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
+
+            </div>
+
+            <?php if ( $media_type === 'image' && $image_url ) : ?>
+                <div class="fcs_media">
+                    <img src="<?= esc_url( $image_url ); ?>" alt="<?= esc_attr( $image_alt ); ?>">
+                </div>
+            <?php elseif ( $media_type === 'video' && $media_video_url ) : ?>
+                <div class="fcs_media">
+                    <div class="embed-responsive">
+                        <iframe src="<?= esc_url( $media_video_url ); ?>" allowfullscreen></iframe>
+                    </div>
+                </div>
+            <?php endif; ?>
+
+            <?php if ( $items && is_array( $items ) && count( $items ) > 0 ) : ?>
+                <div class="fcs_items fcs_items_count_<?= count( $items ); ?>">
+                    <?php foreach ( $items as $item ) :
+                        $type    = isset( $item['item_type'] )       ? $item['item_type']       : 'card';
+                        $i_head  = isset( $item['item_heading'] )    ? $item['item_heading']    : '';
+                        $i_num   = isset( $item['item_number'] )     ? $item['item_number']     : '';
+                        $i_text  = isset( $item['item_text'] )       ? $item['item_text']       : '';
+                        $i_link  = isset( $item['item_link'] )       ? $item['item_link']       : '';
+                        $i_label = isset( $item['item_link_label'] ) ? $item['item_link_label'] : '';
+                    ?>
+                        <div class="fcs_item fcs_item_type_<?= esc_attr( $type ); ?>">
+
+                            <?php if ( $type === 'stat' && $i_num ) : ?>
+                                <p class="fcs_item_number"><?= esc_html( $i_num ); ?></p>
+                            <?php endif; ?>
+
+                            <?php if ( $i_head ) : ?>
+                                <h3 class="fcs_item_heading"><?= esc_html( $i_head ); ?></h3>
+                            <?php endif; ?>
+
+                            <?php if ( $i_text ) : ?>
+                                <p class="fcs_item_text"><?= esc_html( $i_text ); ?></p>
+                            <?php endif; ?>
+
+                            <?php if ( $type === 'linked_card' && $i_link && $i_label ) : ?>
+                                <a href="<?= esc_url( $i_link ); ?>" class="fcs_item_link"><?= esc_html( $i_label ); ?></a>
+                            <?php endif; ?>
+
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
+
         <?php endif; ?>
 
     </div>
